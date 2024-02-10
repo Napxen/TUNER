36197a3b5bbc55c2a28377318ce5122d0b9b56cc
babyUFO <orchideric@yahoo.com>
2024-02-09 21:05:05 -1000

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,6 +1,6 @@
 # Constants
 $TARGET_NODES = 100
-$API_URL = 'https://api.etcnodes.org/peers?all=true'
+$API_URL = 'https://explorer.etcmc-monitor.org/api?port=30303'
 $NODES_FILE = "found_nodes.txt"
 $BATCH_FILE = "START_GETH_FAST_NODE.bat"
 $CONFIG_FILE = "config.toml"
